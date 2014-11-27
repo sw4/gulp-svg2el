819f6119b65f2c873fbf00724cc4dcadb2bd354f
sw4 <sw4@users.noreply.github.com>
2014-11-27 11:33:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var through = require('through2');
-var gutil = require('gulp-util');
-var path = require('path');
-var PluginError = gutil.PluginError;
+var through = require('through2'),
+	gutil = require('gulp-util'),
+	path = require('path'),
+	PluginError = gutil.PluginError;
 const PLUGIN_NAME = 'gulp-jshtml';
 function gulpJshtml(options) {
     return through.obj(function(file, enc, callback){
@@ -17,9 +17,22 @@ function gulpJshtml(options) {
             return callback();
         }
 		options.invoke=options.invoke || 'jshtml';
-        if (file.isBuffer()) {    		
-			var location = file.path,			
-				compiled="\""+String(file.contents).replace(/([^"\\]*(?:\\.[^"\\]*)*)"/g, "$1\\\"").replace(/\r?\n|\r/g, "").replace(/\t/g, '').replace(/\s{2,}/g, ' ').trim()+"\"";
+        if (file.isBuffer()) {   					
+			function escape(s) {
+				return ('' + s) /* Forces the conversion to string. */
+					.replace(/\\/g, '\\\\') /* This MUST be the 1st replacement. */
+					.replace(/\t/g, '\\t') /* These 2 replacements protect whitespaces. */
+					.replace(/\n/g, '\\n')
+					.replace(/\u00A0/g, '\\u00A0') /* Useful but not absolutely necessary. */
+					.replace(/&/g, '\\x26') /* These 5 replacements protect from HTML/XML. */
+					.replace(/'/g, '\\x27')
+					.replace(/"/g, '\\x22')
+					.replace(/</g, '\\x3C')
+					.replace(/>/g, '\\x3E')
+					;
+			}	
+			var location = escape(file.path),			
+				compiled="\""+escape(String(file.contents).replace(/\r?\n|\r/g, "").replace(/\t/g, '').replace(/\s{2,}/g, ' ').trim())+"\"";			
 			compiled=options.invoke+"("+compiled+", \""+location+"\");";
 			file.contents = new Buffer(compiled);  
             this.push(file);
