c0363163f799d74f7e91cc71aa7a20953418f9b6
sw4 <sw4@users.noreply.github.com>
2014-11-27 11:38:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ var through = require('through2'),
 	PluginError = gutil.PluginError;
 const PLUGIN_NAME = 'gulp-jshtml';
 function gulpJshtml(options) {
+  	function escape(s) {
+				return ('' + s) /* Forces the conversion to string. */
+					.replace(/\\/g, '\\\\') /* This MUST be the 1st replacement. */
+					.replace(/\t/g, '\\t') /* These 2 replacements protect whitespaces. */
+					.replace(/\n/g, '\\n')
+					.replace(/\u00A0/g, '\\u00A0') /* Useful but not absolutely necessary. */
+					.replace(/&/g, '\\x26') /* These 5 replacements protect from HTML/XML. */
+					.replace(/'/g, '\\x27')
+					.replace(/"/g, '\\x22')
+					.replace(/</g, '\\x3C')
+					.replace(/>/g, '\\x3E')
+					;
+    }	
     return through.obj(function(file, enc, callback){
         if (file.isNull() || file.isDirectory()) {
             this.push(file);
@@ -18,19 +31,7 @@ function gulpJshtml(options) {
         }
 		options.invoke=options.invoke || 'jshtml';
         if (file.isBuffer()) {   					
-			function escape(s) {
-				return ('' + s) /* Forces the conversion to string. */
-					.replace(/\\/g, '\\\\') /* This MUST be the 1st replacement. */
-					.replace(/\t/g, '\\t') /* These 2 replacements protect whitespaces. */
-					.replace(/\n/g, '\\n')
-					.replace(/\u00A0/g, '\\u00A0') /* Useful but not absolutely necessary. */
-					.replace(/&/g, '\\x26') /* These 5 replacements protect from HTML/XML. */
-					.replace(/'/g, '\\x27')
-					.replace(/"/g, '\\x22')
-					.replace(/</g, '\\x3C')
-					.replace(/>/g, '\\x3E')
-					;
-			}	
+			
 			var location = escape(file.path),			
 				compiled="\""+escape(String(file.contents).replace(/\r?\n|\r/g, "").replace(/\t/g, '').replace(/\s{2,}/g, ' ').trim())+"\"";			
 			compiled=options.invoke+"("+compiled+", \""+location+"\");";
@@ -46,5 +47,5 @@ function gulpJshtml(options) {
             return callback();		
 		}
     });	
-};
+}
 module.exports = gulpJshtml;
