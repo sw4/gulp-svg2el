af02d258e38bf572d8ae2580189be97ed2410a8c
sw4 <sw4@users.noreply.github.com>
2014-11-27 16:40:37 +0000

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp'),
+    pkg = require('./package.json'),
+    plugins = require("gulp-load-plugins")({
+        pattern: ['gulp-*', 'gulp.*'],
+        replaceString: /\bgulp[\-.]/
+    }),
+    banner = ['/**',
+        ' * <%= pkg.title || pkg.name %>',
+        ' * @version v<%= pkg.version %>',
+        ' * @link <%= pkg.homepage %>',
+        ' * @copyright (c)<%= new Date().getFullYear() %> <%= pkg.author.name %>',
+        ' * @license <%= pkg.licenses[0].type %> (<%= pkg.licenses[0].url %>)',
+        ' */',
+        ''
+    ].join('\n');
+
+gulp.task('build:js', function() {
+    return gulp.src('index.js', {
+            base: './'
+        })
+        .pipe(plugins.plumber())
+        .pipe(plugins.jshint())
+        .pipe(plugins.jshint.reporter('jshint-stylish'))
+        .pipe(plugins.header(banner, {
+            pkg: pkg
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', ['browser-sync'], function() {
+    gulp.watch('index.js', ['beautify', 'build:js','bump']);
+    return true;
+});
+gulp.task('beautify', function(){
+  return gulp.src('index.js', {base: './'})
+	.pipe(plugins.plumber())
+	.pipe(plugins.jsbeautifier())
+	.pipe(gulp.dest('./'));
+});
+
+gulp.task('bump', function() {
+    return gulp.src('./package.json')
+        .pipe(plugins.plumber())
+        .pipe(plugins.bump({
+            type: 'patch'
+        }))
+        .pipe(gulp.dest('./'));
+});
+gulp.task('default', ['watch']);
+gulp.task('travis', ['build:styles', 'build:js']);
