e63deb808642e7789f777951cf88064f34e689b8
sw4 <sw4@users.noreply.github.com>
2014-11-27 10:21:56 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,30 +2,13 @@ var through = require('through2');
 var gutil = require('gulp-util');
 var path = require('path');
 var PluginError = gutil.PluginError;
-// consts
 const PLUGIN_NAME = 'gulp-jshtml';
-// plugin level function (dealing with files)
 function gulpJshtml(options) {
     return through.obj(function(file, enc, callback){
-        // Pass file through if:
-        // - file has no contents
-        // - file is a directory
         if (file.isNull() || file.isDirectory()) {
             this.push(file);
             return callback();
         }
-        // User's should be using a compatible glob with plugin.
-        // Example: gulp.src('dir/**/*.{html,htm}').pipe(jshtml())
-		/*
-        if (['.html', '.htm'].indexOf(path.extname(file.path)) === -1) {
-            this.emit('error', new PluginError({
-                plugin: 'JSHTML',
-                message: 'Supported formats include HTML and HTM only.'
-            }));
-            return callback();
-        }
-		*/
-        // No support for streams
         if (file.isStream()) {
             this.emit('error', new PluginError({
                 plugin: 'JSHTML',
@@ -33,14 +16,14 @@ function gulpJshtml(options) {
             }));
             return callback();
         }
-        if (file.isBuffer()) {            
-			var key = path.basename(file.path).replace('.html', '').replace('.htm', ''),
-				varname=options && options.varname ? options.varname : 'jshtml';
-				compiled=varname+"= "+varname+" ? "+varname+" : [];";
-				key=key.indexOf(".")<0 ? key : key.substr(0,key.indexOf("."));
-			compiled+=varname+"['"+key+"']=\""+String(file.contents).replace(/([^"\\]*(?:\\.[^"\\]*)*)"/g, "$1\\\"").replace(/\r?\n|\r/g, "").replace(/\t/g, '').replace(/\s{2,}/g, ' ').trim()+"\";";
-            file.contents = new Buffer(compiled);  
+		options.invoke=options.invoke || 'jshtml';
+        if (file.isBuffer()) {    		
+			var location = file.path,			
+				compiled="\""+String(file.contents).replace(/([^"\\]*(?:\\.[^"\\]*)*)"/g, "$1\\\"").replace(/\r?\n|\r/g, "").replace(/\t/g, '').replace(/\s{2,}/g, ' ').trim()+"\"";
+			compiled=options.invoke+"("+compiled+", \""+location+"\");";
+			file.contents = new Buffer(compiled);  
             this.push(file);
+			console.log(location+ " COMPILED");
             return callback();
         }else{
 			this.emit('error', new PluginError({
@@ -49,8 +32,6 @@ function gulpJshtml(options) {
             }));
             return callback();		
 		}
-    });
-	
+    });	
 };
-// exporting the plugin main function
 module.exports = gulpJshtml;
