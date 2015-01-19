89fc1b67bfdd92dc2aa978fcb88c6aeff4694990
sw4 <sw4@users.noreply.github.com>
2015-01-19 13:53:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,11 @@
  * gulp-svg2el
  * @version v0.0.1
  * @link http://github.com/sw4/gulp-svg2el
- * @copyright (c)2014
+ * @copyright (c)2015 
  * @license MIT (http://github.com/sw4/gulp-svg2el/raw/master/LICENSE-MIT.md)
  */
 var through = require('through2'),
     gutil = require('gulp-util'),
-    path = require('path'),
     PluginError = gutil.PluginError;
 const PLUGIN_NAME = 'gulp-svg2el';
 
@@ -28,12 +27,9 @@ function gulpSvg2el(options) {
         options.invoke = options.invoke || 'svg2el';
         if (file.isBuffer()) {
             var stream = String(file.contents),
-                location = "'" + file.path.replace(/\\/g, "/") + "'",
-                compiled = stream.replace(/(?:\r\n|\r|\n)/g, '').replace(/[\\']/g, "\"").replace(/[\\"']/g, '\\$&').replace(/\t/g, '').replace(/\s{2,}/g, ' ').trim();
-            compiled = options.invoke + "(" + location + ", \'" + compiled + "\');";
+				compiled=stream.substr(stream.indexOf("<svg"), stream.length);
             file.contents = new Buffer(compiled);
             this.push(file);
-            console.log(location + " COMPILED");
             return callback();
         } else {
             this.emit('error', new PluginError({
